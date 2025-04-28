fd700454176021df33c290f802600a406bc1dbd9
Zhukov Sergei <alivesystemsonly@gmail.com>
2025-04-28 05:42:28 +0200

    Typed DateHelper

--- app/api/helpers/DateHelper.php
+++ app/api/helpers/DateHelper.php
@@ -4,7 +4,13 @@ namespace api\helpers;
 
 class DateHelper
 {
-    public static function formatApi($timestamp)
+    /**
+     * Formats a Unix timestamp into RFC3339 format for API responses.
+     *
+     * @param int|null $timestamp The Unix timestamp to format.
+     * @return string|null The formatted date string or null if the input timestamp is null/falsy.
+     */
+    public static function formatApi(?int $timestamp): ?string
     {
         return $timestamp ? date(DATE_RFC3339, $timestamp) : null;
     }
